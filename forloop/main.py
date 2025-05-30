3daf79eeceff796571d41ec5d8baded31857b6d5
varshini-1904 <varshini-1904@example.com>
2025-05-30 15:50:05 +0530

    forloop

---
+++ forloop/main.py
@@ -0,0 +1,9 @@
+s=int(input("enter a num"))
+rev=0
+while s>0:
+    r=s%10
+    rev=rev*10+r
+    s=s56//10
+print(rev)
+    
+
---
+++ forloop/multof nums.py
@@ -0,0 +1,5 @@
+num=int(input("entear a num"))
+i=0
+while i<10:
+    i=i+1
+    print(num,"*",i,"=",num*i)
